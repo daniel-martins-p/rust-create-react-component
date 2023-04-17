e8949ec90fc21bdd95950e01ca63b7b70b864a59
Daniel Martins <mp.daniel.414@gmail.com>
2023-04-16 21:56:22 -0300

    Changes to 1.0

--- src/models/create_react_component_args.rs
+++ src/models/create_react_component_args.rs
@@ -1,7 +1,7 @@
 use clap:: {Parser};
 
 #[derive(Debug, Parser)]
-#[clap(version = "0.0.1", about = "Scafolds a component folder structure with a index export file, the component file and the unit test file", author = "Daniel Martins <mp.daniel.414@gmail.com")]
+#[clap(version = "0.1.0", about = "Scafolds a component folder structure with a index export file, the component file and the unit test file", author = "Daniel Martins <mp.daniel.414@gmail.com")]
 pub struct CreateReactComponentArgs {
   /// Component Name - Which will be used as the component name and the folder name
   #[arg(short = 'n', long = "name", required = true)]
